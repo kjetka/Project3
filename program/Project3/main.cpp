518e7aa4c074dac73a4892d770bc500942b24dc2
Vilde <vildemjo@uio.no>
2017-10-10 15:01:14 +0200

    Revert "fixing to be able to pull"
    This reverts commit 404c6d2fc43fb2c9df4fed58eff1115ed2b175d1.

--- program/Project3/main.cpp
+++ program/Project3/main.cpp
@@ -12,94 +12,9 @@ int main(){
     Planet sun(1, 0,0,0,0);
     Solver testing;
 
-<<<<<<< Updated upstream
     testing.add(earth);
     testing.add(sun);
     testing.alt();
-=======
-    Solver* solv;
-
-    double x, y, vx, vy;
-
-    solv = new Solver();
-    solv->velocityVerlet(x, y, vx, vy);
-
-
-  //  solv->
-/*
-    double timeLimit = 1.0;
-    int numberofsteps = 10000;
-    double time = 0;
-    double dt = timeLimit/numberofsteps;
-
-    double pi = acos(-1.0);
-
-    double x = 1.0;
-    double y = 0.0;
-    double vx = 0;
-    double vy = 2*pi;
-    double ax, ay, r;
-
-
-    double fourpi2 = 4*pi*pi;
-
-    // Euler-Cromer method:
-    while(time<=timeLimit){
-        r = sqrt(pow(x,2) + pow(y,2));
-
-        ax = (-fourpi2/pow(r,3))*x;
-        ay = (-fourpi2/pow(r,3))*y;
-
-        vx = vx + dt*ax;
-        vy = vy + dt*ay;
-
-        x = x + dt*vx;
-        y = y + dt*vy;
-
-        time = time + dt;
-    }
-
-    cout << "Euler:" << endl;
-    cout << "Position: " << "(" << x << " , " << y << " ) " << endl;
-    cout << "Velocity: " << "(" << vx << " , " << vy << " ) " << endl;
-
-    x = 1.0;
-    y = 0.0;
-    vx = 0;
-    vy = 2*pi;
-
-    // Velocity Verlet:
-
-    double dtdiv2 = dt/2;
-
-    double ax_new, ay_new, vx_half, vy_half;
-
-    r = sqrt(pow(x,2) + pow(y,2));
-    ax = (-fourpi2/pow(r,3))*x;
-    ay = (-fourpi2/pow(r,3))*y;
-
-    while(time<=timeLimit){
-        vx_half = vx + dtdiv2*ax;
-        vy_half = vy + dtdiv2*ay;
-
-        x = x + dt*vx_half;
-        y = y + dt*vy_half;
-
-        r = sqrt(pow(x,2) + pow(y,2));
-
-        ax = (-fourpi2/pow(r,3))*x;
-        ay = (-fourpi2/pow(r,3))*y;
-
-        vx = vx_half + dtdiv2*ax;
-        vy = vy_half + dtdiv2*ay;
-
-        time = time + dt;
-    }
-*/
-std::cout << "Velocity Verlet:"<<std::endl;
-std::cout << "Position: " << "(" << x << " , " << y << " ) " << std::endl;
-std::cout << "Velocity: " << "(" << vx << " , " << vy << " ) " << std::endl;
->>>>>>> Stashed changes
 
     return 0;
 }
